7578dacf13cdfcdff040a95aec19385c492eabf3
juan rodriguez <f4ast_cb@hotmail.com>
2025-11-01 01:06:26 -0500

    Título NETLINK PERÚ en color azul corporativo

--- routes/reportes.routes.js
+++ routes/reportes.routes.js
@@ -82,7 +82,7 @@ async function addHeader(doc, title, filtro = {}) {
   if (filtro.desde || filtro.hasta) {
     doc.moveDown(0.8); // más espacio antes del rango
     doc.font("Helvetica")
-      .fontSize(10)
+      .fontSize(9)
       .text(`Rango: ${filtro.desde || "---"} hasta ${filtro.hasta || "---"}`, { align: "center" });
   }
 
